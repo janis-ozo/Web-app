785581b444ac8dd3c06270fab77edf2b56415490
janis-ozo <janis_O@hotmail.com>
2020-04-18 11:05:36 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+require_once __DIR__. "/vendor/autoload.php";
+
+
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $router) {
+    $router->addRoute('GET', '/users', 'UsersController@index');
+    // {id} must be a number (\d+)
+    $router->addRoute('GET', '/users/{id:\d+}', 'UsersController@show');
+//    // The /{title} suffix is optional
+//    $router->addRoute('GET', '/articles/{id:\d+}[/{title}]', 'UserController@index');
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $params = $routeInfo[2];
+
+        // ... call $handler with $vars
+        [$controller, $method] = explode('@', $handler);
+        $controllerPath = '\App\Controllers\\' . $controller;
+        echo (new $controllerPath)->{$method}($params);
+
+        break;
+}
